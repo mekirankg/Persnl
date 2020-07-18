143ea61132af227b4ee54347c01680c93be5463a
kiran.girija <kiran.girija@quest-global.com>
2020-07-18 19:52:03 +0530

    about popup added
    Signed-off-by: kiran.girija <kiran.girija@quest-global.com>

--- MyMirror/app/src/main/java/com/app/sk/kmirror/MainActivity.java
+++ MyMirror/app/src/main/java/com/app/sk/kmirror/MainActivity.java
@@ -3,11 +3,17 @@ package com.app.sk.kmirror;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuCompat;
 
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,11 +23,13 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    PopupWindow popUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        popUp = new PopupWindow(this);
         this.getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setDisplayShowCustomEnabled(true);
         getSupportActionBar().setCustomView(R.layout.custom_action_bar);
@@ -34,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "You have clicked tittle", Toast.LENGTH_LONG).show();
             }
         });
+
+
     }
 
     public void cameraClicked(View view) {
@@ -45,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
+        MenuCompat.setGroupDividerEnabled(menu, true);
         inflater.inflate(R.menu.menu_item, menu);
         return true;
     }
@@ -53,14 +64,46 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.menu_about:
-                Toast.makeText(MainActivity.this, "About clicked", Toast.LENGTH_LONG).show();
+               // Toast.makeText(MainActivity.this, "About clicked", Toast.LENGTH_LONG).show();
+                showAboutWindow();
                 return (true);
-            case R.id.menu_version:
+         /*   case R.id.menu_version:
                 Toast.makeText(MainActivity.this, R.string.app_version, Toast.LENGTH_LONG).show();
-                return (true);
+                return (true);*/
             default:
 
         }
         return (super.onOptionsItemSelected(item));
+    }
+
+    private void showAboutWindow() {
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.about_popup, null);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(getWindow().getDecorView().getRootView(), Gravity.CENTER, 0, 0);
+        Button bt = popupView.findViewById(R.id.ok_button);
+        bt.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                popupWindow.dismiss();
+            }
+        });
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
     }
 }
