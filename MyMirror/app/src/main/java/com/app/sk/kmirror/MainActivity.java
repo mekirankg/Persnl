2d52ffc308b936e0101305f235016ff72df23012
kiran.girija <kiran.girija@quest-global.com>
2020-07-18 13:34:21 +0530

    menu bar added
    Signed-off-by: kiran.girija <kiran.girija@quest-global.com>

--- MyMirror/app/src/main/java/com/app/sk/kmirror/MainActivity.java
+++ MyMirror/app/src/main/java/com/app/sk/kmirror/MainActivity.java
@@ -1,26 +1,66 @@
 package com.app.sk.kmirror;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button camera_open_id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        camera_open_id = (Button)findViewById(R.id.camera_button);
+        this.getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(R.layout.custom_action_bar);
+        //getSupportActionBar().setElevation(0);
+        View view = getSupportActionBar().getCustomView();
+        TextView name = view.findViewById(R.id.name);
+        name.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You have clicked tittle", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     public void cameraClicked(View view) {
         Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
         intent.putExtra("android.intent.extras.CAMERA_FACING", 1);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_item, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_about:
+                Toast.makeText(MainActivity.this, "About clicked", Toast.LENGTH_LONG).show();
+                return (true);
+            case R.id.menu_version:
+                Toast.makeText(MainActivity.this, R.string.app_version, Toast.LENGTH_LONG).show();
+                return (true);
+            default:
+
+        }
+        return (super.onOptionsItemSelected(item));
     }
 }
